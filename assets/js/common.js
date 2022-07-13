733272fdc3b1c7ab06dc290060cc9cad142a88af
eeuneeun <31426680+eeuneeun@users.noreply.github.com>
2022-07-13 17:07:55 +0900

    최종 저장 전

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,20 +1,33 @@
-function codeWrapToggle(){
-  const codeToggleBtn = document.querySelectorAll('.code_toggle_btn')
+function codeWrapToggle() {
+  const codeToggleBtn = document.querySelectorAll(".code_toggle_btn");
 
   codeToggleBtn &&
-  codeToggleBtn.forEach(item => {
-    item.addEventListener('click', (e)=>{
-      item.innerHTML == '열기' 
-      ? item.innerHTML = '닫기'
-      : item.innerHTML = '열기'
-      
-      e.target.parentElement.classList.toggle('active')
-    })
-  });
+    codeToggleBtn.forEach((item) => {
+      item.addEventListener("click", (e) => {
+        item.innerHTML == "열기"
+          ? (item.innerHTML = "닫기")
+          : (item.innerHTML = "열기");
+
+        e.target.parentElement.classList.toggle("active");
+      });
+    });
 }
 
+function contentsToggle() {
+  const contentsToggleBtn = document.querySelectorAll(
+    ".contents .list > li h4"
+  );
+
+  contentsToggleBtn &&
+    contentsToggleBtn.forEach((item) => {
+      item.addEventListener("click", (e) => {
+        item.classList.toggle("active");
+      });
+    });
+}
 
 document.addEventListener("DOMContentLoaded", () => {
-  codeWrapToggle()
+  contentsToggle();
+  codeWrapToggle();
   hljs.highlightAll();
-})
+});
